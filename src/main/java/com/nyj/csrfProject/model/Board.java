3a457288294661c49e84c87b16ce13e15c80f05b
skduswn <nyj5458@gmail.com>
2021-11-20 20:52:25 +0900

    db설정 완료

--- src/main/java/com/nyj/csrfProject/model/Board.java
+++ src/main/java/com/nyj/csrfProject/model/Board.java
@@ -1,5 +1,37 @@
 package com.nyj.csrfProject.model;
 
-public class Board {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Board {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+	private int id; 
+	
+	@Column(nullable = false, length = 100)
+	private String title;
+	
+	@Lob // 대용량 데이터
+	private String content; // 섬머노트 라이브러리 <html>태그가 섞여서 디자인이 됨.
+
+	@ManyToOne(fetch = FetchType.EAGER)  // Many = Many,  User = One
+	@JoinColumn(name="userId")
+	private User user;
 }
